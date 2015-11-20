228bc2ccc5066083a5f7cc8b1db0a273fb82c0dc
IgorBMSTU <igor95n@gmail.com>
2015-11-20 16:53:18 +0300

    modified:   ansible/roles/test/templates/find-N.py.j2 	modified:   tests_run.py

--- tests_run.py
+++ tests_run.py
@@ -5,7 +5,6 @@ import getpass
 
 from tempfile import NamedTemporaryFile
 from ansible.playbook import PlayBook
-from ansible.inventory import Inventory
 from ansible import callbacks
 from ansible import utils
 
@@ -13,25 +12,22 @@ run_type_opt = ["apache", "uwsgi"]
 db_opt = ["mysql", "postgresql"]
 fs_opt = ["tmpfs", "/dev/sdb", "dev/sdc"]
 
+os_username = "ubuntu"
+
 OPT = []
 password = getpass.getpass()
 
-def run_playbook(name, extra):
 
+def run_playbook(name, extra):
     utils.VERBOSITY = 0
     playbook_cb = callbacks.PlaybookCallbacks(verbose=utils.VERBOSITY)
     stats = callbacks.AggregateStats()
-    runner_cb = callbacks.PlaybookRunnerCallbacks(stats, verbose=utils.VERBOSITY)
+    runner_cb = callbacks.PlaybookRunnerCallbacks(stats,
+                                                  verbose=utils.VERBOSITY)
 
     inventory = """
     [customer]
     {{ public_ip_address }}
-
-    [customer:vars]
-    domain={{ domain_name }}
-    customer_id={{ customer_id }}
-    customer_name={{ customer_name }}
-    customer_email={{ customer_email }}
     """
 
     inventory_template = jinja2.Template(inventory)
@@ -47,60 +43,63 @@ def run_playbook(name, extra):
     hosts.write(rendered_inventory)
     hosts.close()
     pb = PlayBook(
-        playbook='/home/ubuntu/keystone_full_deployment/ansible/%s.yml'% name,
-        host_list=hosts.name,  
-        remote_user='ubuntu',
+        playbook='/home/%s/keystone_full_deployment/ansible/%s.yml'
+                 % (os_username, name),
+        host_list=hosts.name,
+        remote_user=os_username,
         callbacks=playbook_cb,
         runner_callbacks=runner_cb,
         stats=stats,
-        private_key_file='/home/ubuntu/.ssh/id_rsa',
-        become=True, 
-        become_pass = "%s\n" % password,
-        become_method = 'sudo',
-        extra_vars = extra
-    
+        private_key_file='/home/%s/.ssh/id_rsa' % os_username,
+        become=True,
+        become_pass="%s\n" % password,
+        become_method='sudo',
+        extra_vars=extra
     )
     results = pb.run()
     playbook_cb.on_stats(pb.stats)
     os.remove(hosts.name)
     return results
 
+
 def get_fs_type(src):
     if "tmpfs":
         return "tmpfs"
     return "ext4"
 
+
 test_num = 1
 for run_type in run_type_opt:
     for db in db_opt:
         for fs_src in fs_opt:
-            PARAMS = {"global_run_type" : run_type,
-                      "global_db" : db,
-                      "global_fs_type" : get_fs_type(fs_src),
-                      "global_fs_src" : fs_src,
-                      "test_num" : str(test_num)
+            PARAMS = {"global_run_type": run_type,
+                      "global_db": db,
+                      "global_fs_type": get_fs_type(fs_src),
+                      "global_fs_src": fs_src,
+                      "test_num": str(test_num)
                       }
             params_s = json.dumps(PARAMS)
             OPT.append(PARAMS)
             test_num += 1
 
+
 def install():
-    sh_text = "ansible-playbook -i hosts -K install_all.yml"
     run_playbook("install_all", {})
 
+
 def stop(params):
-    sh_text = "ansible-playbook -i hosts -K stop_all.yml --extra-vars '%s'" % params
-    run_playbook("stop_all",params)
+    run_playbook("stop_all", params)
+
 
 def run_deps():
     for params in OPT:
         stop(params)
-        sh_text = "ansible-playbook -i hosts -K run_dep.yml --extra-vars '%s'" % params
         run_playbook("run_dep", params)
-        test_sh = "ansible-playbook -i hosts -K run_tests.yml --extra-vars '%s'" % params
         run_playbook("run_tests", params)
         stop(params)
 
+
 os.chdir("./ansible/")
-#install()
+install()
 run_deps()
+
